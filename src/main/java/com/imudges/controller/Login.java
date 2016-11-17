2042bf620acf5f4cf2a51e4348cb43cbccb4958d
cyy <chenyangyang@imudges.com>
2016-11-17 16:26:57 +0800

    注册功能2

--- src/main/java/com/imudges/controller/Login.java
+++ src/main/java/com/imudges/controller/Login.java
@@ -1,26 +1,37 @@
 package com.imudges.controller;
 
+import com.imudges.model.UserEntity;
+import com.imudges.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by cyy on 2016/10/26.
  */
 @Controller
 public class Login {
+    @Autowired
+    private UserRepository userRepository;
     public String Email;
     public String PassWord;
     @RequestMapping(value = "/login.html",method = RequestMethod.GET)
     public String login(){
         return "login";
     }
-    @ResponseBody
+    //@ResponseBody
     @RequestMapping(value = "/user_login",method = RequestMethod.POST)
-    public String Get(String email,String password){
-        Email=email;
-        PassWord=password;
-        return email;
+    public String  Verification(String email,String password){
+        UserEntity userEntity=userRepository.findByEmail(email) ;
+        if(userEntity==null){
+            return "login";
+        }
+        else{
+            if(userEntity.getPassword()==password)
+                return "index";
+            else
+                return "login";
+        }
     }
 }
